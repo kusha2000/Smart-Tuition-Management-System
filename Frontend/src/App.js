ffd03bd0edce5a061838722cedfd7ba00ba1e94e
kusha2000 <kushanandarawewa1@gmail.com>
2024-12-06 10:16:11 +0530

    Errors Fixed for React Script 5.0

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,51 +1,61 @@
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
-import { Route, Switch, Redirect } from "react-router-dom";
-import {Amplify} from 'aws-amplify';
-import { Authenticator,withAuthenticator } from "@aws-amplify/ui-react";
-import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Authenticator>
-          {({signOut,user})=>(
-            <main>
-              <div className="App">
-                <header className="App-header">
-                  <button onClick={signOut}>Sign Out</button>
-                  <h2>My App Content</h2>
-                </header>
-              </div>
-            </main>
-          )}
-        </Authenticator>
-      </header>
-    </div>
-  );
-}
-
-export default withAuthenticator(App);
+//====================================
+//Amplify and Cognito Default Template
+//====================================
 
 // import SignUp from "./components/SignUp";
 // import Login from "./components/Login";
 // import { Route, Switch, Redirect } from "react-router-dom";
+// import {Amplify} from 'aws-amplify';
+// import { Authenticator,withAuthenticator } from "@aws-amplify/ui-react";
+// import '@aws-amplify/ui-react/styles.css';
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
 
 // function App() {
 //   return (
-//     <>
-//       <Switch>
-//         <Route path="/login" component={Login} />
-//         <Route path="/signup" component={SignUp} />
-//         <Redirect from="/" to="/signup" />
-//       </Switch>
-//     </>
+//     <div className="App">
+//       <header className="App-header">
+//         <Authenticator>
+//           {({signOut,user})=>(
+//             <main>
+//               <div className="App">
+//                 <header className="App-header">
+//                   <button onClick={signOut}>Sign Out</button>
+//                   <h2>My App Content</h2>
+//                 </header>
+//               </div>
+//             </main>
+//           )}
+//         </Authenticator>
+//       </header>
+//     </div>
 //   );
 // }
 
-// export default App;
+// export default withAuthenticator(App);
+
+
+//====================================
+//Our UI
+//====================================
+
+
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Redirect from="/" to="/signup" />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
