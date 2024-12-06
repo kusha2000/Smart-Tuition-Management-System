1f31e81009d0406f2022cdbaf7761d2b7a29b0d3
kusha2000 <kushanandarawewa1@gmail.com>
2024-12-06 10:05:30 +0530

    Configure the AWS Amplify and Cognito

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,51 @@
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import { Route, Switch, Redirect } from "react-router-dom";
+import {Amplify} from 'aws-amplify';
+import { Authenticator,withAuthenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 
 function App() {
   return (
-    <>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Redirect from="/" to="/signup" />
-      </Switch>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <Authenticator>
+          {({signOut,user})=>(
+            <main>
+              <div className="App">
+                <header className="App-header">
+                  <button onClick={signOut}>Sign Out</button>
+                  <h2>My App Content</h2>
+                </header>
+              </div>
+            </main>
+          )}
+        </Authenticator>
+      </header>
+    </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+// import SignUp from "./components/SignUp";
+// import Login from "./components/Login";
+// import { Route, Switch, Redirect } from "react-router-dom";
+
+
+// function App() {
+//   return (
+//     <>
+//       <Switch>
+//         <Route path="/login" component={Login} />
+//         <Route path="/signup" component={SignUp} />
+//         <Redirect from="/" to="/signup" />
+//       </Switch>
+//     </>
+//   );
+// }
+
+// export default App;
