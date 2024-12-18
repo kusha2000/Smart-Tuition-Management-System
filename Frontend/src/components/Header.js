c0652ce3e05a1a4500a42743886b85801c065b06
kusha2000 <kushanandarawewa1@gmail.com>
2024-12-19 00:56:36 +0530

    Correct AWS Authentication Errors

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "./Header.css";
 
-const Header = () => {
+const Header = ({ signOutFun }) => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Local state to manage login status
   const [userName, setUserName] = useState("Guest"); // Example user name for display
@@ -18,27 +19,17 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand>Smart Tuition Management System</Navbar.Brand>
+          {/* Brand Title */}
+          <Navbar.Brand className="navbar-brand">
+            Smart Tuition Management System
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              {isLoggedIn ? (
-                <>
-                  <Nav.Link>{userName}</Nav.Link>
-                  <LinkContainer to="/">
-                    <Nav.Link onClick={logoutHandler}>Logout</Nav.Link>
-                  </LinkContainer>
-                </>
-              ) : (
-                <>
-                  <LinkContainer to="/login">
-                    <Nav.Link>Login</Nav.Link>
-                  </LinkContainer>
-                  <LinkContainer to="/register">
-                    <Nav.Link>Register</Nav.Link>
-                  </LinkContainer>
-                </>
-              )}
+            <Nav>
+              {/* Home Button */}
+              <LinkContainer to="/">
+              <button onClick={signOutFun} className="logoutbutton">Sign Out</button>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
